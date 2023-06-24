0f22908b580d1a1adf4ac67dde24caed28ff3519
joaorodriguesjr <mail@joaorodriguesjr.com>
2023-06-24 12:01:59 -0300

    add storage functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,19 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { Layer } from './Layer'
+import { Neuron } from './Neuron'
+import { Network } from './Network'
+
+export function store(network: Network): void {
+  const data = network.layers.map(layer => layer.neurons.map(neuron => ({ weights: neuron.weights, bias: neuron.bias })))
+  writeFileSync('data/trained.json', JSON.stringify(data, null, 2))
+}
+
+export function restore(): Network {
+  const data = JSON.parse(readFileSync('data/trained.json').toString())
+  const layers = data.map(layer => new Layer(layer.map(neuron => new Neuron(neuron.weights, neuron.bias))))
+  return new Network(layers)
+}
+
 export function iterate(count: number, callback: (index: number) => void): void {
   for (let index = 0; index < count; index++) callback(index)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { Network } from './Network'
-import { iterate } from './helpers'
+import { iterate, store, restore } from './helpers'
 
-const network = Network.create([
-  [2, 2], [2, 2], [2, 1]
-])
+// const network = Network.create([
+//   [2, 2], [2, 2], [2, 1]
+// ])
 
-iterate(100000, () => {
-  network.train([0, 0], [0], 0.1)
-  network.train([1, 1], [0], 0.1)
-  network.train([0, 1], [1], 0.1)
-  network.train([1, 0], [1], 0.1)
+// iterate(100000, (iteration) => {
+//   network.train([0, 0], [0], 0.1)
+//   network.train([1, 1], [0], 0.1)
+//   network.train([0, 1], [1], 0.1)
+//   network.train([1, 0], [1], 0.1)
+// })
+
+// store(network)
+
+const network = restore()
+
+iterate(10, () => {
+  console.log(
+    Math.round(network.predict([0, 0])[0]),
+    Math.round(network.predict([1, 1])[0]),
+    Math.round(network.predict([1, 0])[0]),
+    Math.round(network.predict([0, 1])[0])
+  )
 })
-
-console.log(
-  Math.round(network.predict([0, 0])[0]),
-  Math.round(network.predict([1, 1])[0]),
-  Math.round(network.predict([1, 0])[0]),
-  Math.round(network.predict([0, 1])[0])
-)
