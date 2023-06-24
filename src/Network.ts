c819ab52da4d26c289f133e2ac3122917ae1e8c4
joaorodriguesjr <mail@joaorodriguesjr.com>
2023-06-24 09:54:05 -0300

    extract gradient calculation

--- src/Network.ts
+++ src/Network.ts
@@ -3,21 +3,27 @@ import { Layer } from './Layer'
 export class Network {
   public constructor(public layers: Layer[]) {}
 
-  public train(inputs: number[], targets: number[], learningRate: number): void {
-    const outputs = this.predict(inputs)
-
-    this.layers.reverse().forEach((layer, index) => {
-      if (index === 0)
-        layer.calculateOutputGradients(targets)
-      else
-        layer.calculateHiddenGradients(this.layers[index - 1])
-    })
-
-    this.layers.reverse().forEach((layer) => layer.updateWeights(learningRate))
-  }
-
   public predict(inputs: number[]): number[] {
     return this.layers.reduce((inputs, layer) => layer.process(inputs), inputs)
+  }
+
+  public train(inputs: number[], targets: number[], learningRate: number): void {
+    this.predict(inputs)
+    this.calculateGradients(targets)
+
+    this.layers.forEach((layer) => layer.updateWeights(learningRate))
+  }
+
+  private calculateGradients(targets: number[]): void {
+    this.layers.reverse()
+
+    this.layers.forEach((layer, index) => {
+      (! index)
+        ? layer.calculateOutputGradients(targets)
+        : layer.calculateHiddenGradients(this.layers[index - 1])
+    })
+
+    this.layers.reverse()
   }
 
   public static create(structure: number[][]): Network {
